20a76ad7b9d173acec89dd181cd721b52e46a511
Rushikesh <rxrushi561@gmail.com>
2026-02-21 11:37:27 +0530

    backend ready for deploy

--- Backend_FD/server.js
+++ Backend_FD/server.js
@@ -36,6 +36,7 @@ app.use("/api/cart", cartRoute)
 app.use("/api/order",orderRoute)
 
 //server create
-app.listen(3000, () => {
+const port =process.env.PORT || 3000;
+app.listen(port, () => {
   console.log("Server running on port 3000");
 });
